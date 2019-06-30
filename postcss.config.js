ecbf8bd057a8751233a81222417a8aed8517e808
danhtran94 <dd2672015@gmail.com>
2019-06-30 13:27:30 +0700

    eslint

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-/* eslint "import/no-extraneous-dependencies": off */
+/* eslint-disable */
 const AutoPrefixerPlugin = require("autoprefixer");
 const CSSNano = require("cssnano")({
   preset: "default"
